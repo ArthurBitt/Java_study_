fe3189d4a15718344f99c79e0fa9d161f8483761
ArthurBitt <add2coffee@gmail.com>
2024-01-22 23:08:34 -0300

    .

--- Curso_Java/java_fundamentos/src/oop/interfaces/luxo.java
+++ Curso_Java/java_fundamentos/src/oop/interfaces/luxo.java
@@ -4,7 +4,7 @@ public interface luxo {
 
     void ligarAr();
     void desligarAr();
-    
+
     // É possível deixar um valor padrao em um método da interface
     default int velocidadeAr(){
         return 1;
